9fb4308ef5fc808e03653e605e8c3446f1bd096e
MShahzaib0512 <146122274+MShahzaib0512@users.noreply.github.com>
2024-09-11 12:48:55 +0500

    Perform some celery tasks

--- apk1/views.py
+++ apk1/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from celeryapp.celery import add
+from celery.result import AsyncResult
 # Create your views here.
 def index(request):
- print(add.delay(10,20))
- return render(request,"Apk1\home.html")
+ result = add.delay(10,20)
+ return render(request,"Apk1\home.html", {'result':result})
+def results(request, task_id):
+    result = AsyncResult(task_id)
+    return render(request, 'Apk1/result.html', {'result': result})
+
 def about(request):
  return render(request,"Apk1\About.html")
 def contact(request):
--- celeryapp/celery.py
+++ celeryapp/celery.py
@@ -1,21 +1,22 @@
+# celery.py
 import os
-
 from celery import Celery
 from time import sleep
 
-# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryapp.settings')
 
 app = Celery('celeryapp')
+app.conf.update(
+    broker_url='redis://127.0.0.1:6379/0',
+    result_backend='redis://127.0.0.1:6379/0',
+    broker_transport_options={
+        'visibility_timeout': 3600,  # Adjust as needed
+    },
+)
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
 
 @app.task
 def add(x,y):
